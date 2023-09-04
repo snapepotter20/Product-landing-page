e005438a906dbb96fad196a46e4b54c43bbecedd
Sahil Kumar <67713177+snapepotter20@users.noreply.github.com>
2023-09-04 12:59:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navigation from './Components/Navigation';
+import Hero from './Components/Hero';
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+      <Hero/>
+    </div>
+  );
+}
+
+export default App;
